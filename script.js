4bb5162a477adfd802c31c378e0f2aff0ddeadbc
Danny575-ui <danushp.016@gmail.com>
2024-09-15 13:56:55 +0530

    finish

--- script.js
+++ script.js
@@ -1,128 +1,160 @@
 let a = [];
 let k = 0;
+let b = [];
+let num = 0;
 
 function clickedOne() {
-  a[k] = 1;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(1);
 }
 
 function clickedTwo() {
-  a[k] = 2;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(2);
 }
 
 function clickedThree() {
-  a[k] = 3;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(3);
 }
 
 function clickedFour() {
-  a[k] = 4;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(4);
 }
 
 function clickedFive() {
-  a[k] = 5;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(5);
 }
 
 function clickedSix() {
-  a[k] = 6;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(6);
 }
 
 function clickedSeven() {
-  a[k] = 7;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(7);
 }
 
 function clickedEight() {
-  a[k] = 8;
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  appendNumber(8);
 }
 
 function clickedNine() {
-  a[k] = 9;
+  appendNumber(9);
+}
+
+function clickedZero() {
+  appendNumber(0);
+}
+
+function appendNumber(value) {
+  a[k] = value;
   document.getElementById("result").innerHTML = a[k];
   k++;
 }
 
-function clickedZero() {
-  if (a[k--] == "/") {
-    document.getElementById("result").innerHTML = "Undefined";
-    a = [];
-    k = 0;
-  } else {
-    a[k] = 0;
-    document.getElementById("result").innerHTML = a[k];
-    k++;
-  }
-}
-
 function clickedPlus() {
-  if (a[k--] == "+" || a[k--] == "-" || a[k--] == "*" || a[k--] == "/") {
-    document.getElementById("result").innerHTML = "Improper question format";
-    a = [];
-    k = 0;
-  } else {
-    a[k] = "+";
-    document.getElementById("result").innerHTML = a[k];
-    k++;
-  }
+  appendOperator("+");
 }
 
 function clickedMinus() {
-  if (a[k--] == "+" || a[k--] == "-" || a[k--] == "*" || a[k--] == "/") {
-    document.getElementById("result").innerHTML = "Improper question format";
-    a = [];
-    k = 0;
-  } else {
-    a[k] = "-";
-    document.getElementById("result").innerHTML = a[k];
-    k++;
-  }
+  appendOperator("-");
 }
 
 function clickedMult() {
-  if (a[k--] == "+" || a[k--] == "-" || a[k--] == "*" || a[k--] == "/") {
-    document.getElementById("result").innerHTML = "Improper question format";
-    a = [];
-    k = 0;
-  } else {
-    a[k] = "*";
-    document.getElementById("result").innerHTML = a[k];
-    k++;
-  }
+  appendOperator("*");
 }
 
 function clickedDiv() {
-  if (a[k--] == "+" || a[k--] == "-" || a[k--] == "*" || a[k--] == "/") {
+  appendOperator("/");
+}
+
+function appendOperator(operator) {
+  if (k === 0 || typeof a[k - 1] === "string") {
     document.getElementById("result").innerHTML = "Improper question format";
-    a = [];
-    k = 0;
+    resetCalculator();
   } else {
-    a[k] = "/";
+    a[k] = operator;
     document.getElementById("result").innerHTML = a[k];
     k++;
   }
 }
 
 function clickedEqualto() {
-  a[k] = "=";
-  document.getElementById("result").innerHTML = a[k];
-  k++;
+  num = joinDigits();
+  solveExpression();
 }
 
 function clickedAllclear() {
-  a = [];
+  resetCalculator();
   document.getElementById("result").innerHTML = 0;
+}
+
+function resetCalculator() {
+  a = [];
+  b = [];
   k = 0;
+  num = 0;
+}
+
+function joinDigits() {
+  let temp = "";
+  let l = 0;
+
+  for (let i = 0; i < k; i++) {
+    if (typeof a[i] === "number") {
+      temp += a[i];
+    }
+    if (a[i] === "+" || a[i] === "-" || a[i] === "*" || a[i] === "/") {
+      b[l++] = parseFloat(temp);
+      b[l++] = a[i];
+      temp = "";
+    }
+  }
+
+  if (temp) {
+    b[l] = parseFloat(temp);
+  }
+
+  num = l - 1;
+
+  return num;
+}
+
+function solveExpression() {
+  if (b.length === 0) {
+    document.getElementById("result").innerHTML = "Error: Empty Expression";
+    resetCalculator();
+    return;
+  }
+
+  let result = b[0];
+
+  for (let i = 1; i < b.length; i += 2) {
+    let operator = b[i];
+    let nextNumber = b[i + 1];
+
+    switch (operator) {
+      case "+":
+        result += nextNumber;
+        break;
+      case "-":
+        result -= nextNumber;
+        break;
+      case "*":
+        result *= nextNumber;
+        break;
+      case "/":
+        if (nextNumber === 0) {
+          document.getElementById("result").innerHTML = "Undefined";
+          resetCalculator();
+          return;
+        }
+        result /= nextNumber;
+        break;
+      default:
+        document.getElementById("result").innerHTML = "Error: Unknown operator";
+        resetCalculator();
+        return;
+    }
+  }
+
+  document.getElementById("result").innerHTML = result;
+  resetCalculator();
 }
